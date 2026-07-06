53cf1c399b6d93ba93af3b08f3db000ecc3e8680
Durgesh <durgeshnidadavolu@gmail.com>
2026-07-06 22:56:00 +0530

    added the id selector for the event delegation pattren

--- 02-like-counter/js/script.js
+++ 02-like-counter/js/script.js
@@ -5,3 +5,21 @@
 //   #likeBtn    — 👍 Like
 //   #unlikeBtn  — 👎 Unlike
 //   #resetBtn   — 🔄 Reset
+
+// Grab the elements using any selector: #id, .class, button, etc.
+
+const getEl = (selector) => document.querySelector(selector);
+
+// Display Count Number
+
+const countDisplay = () => {    
+    return getEl("#likeCount");
+}
+
+// Eventlistners to attached and perform actions
+
+// Like Count
+
+// Unlike Count
+
+// Reset Count
