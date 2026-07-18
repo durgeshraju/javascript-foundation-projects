3c78ce7eb87d12b1c3989317e75df1eb0017322b
Durgesh <durgeshnidadavolu@gmail.com>
2026-07-18 12:55:12 +0530

    Split add/remove list project files

---
+++ 03-add-remove-list-items/js/script.js
@@ -0,0 +1,22 @@
+// Add and Remove List Items - your JavaScript goes here.
+//
+// Available elements:
+//   #itemInput     - the text input
+//   #addItemBtn    - the add button
+//   #errorMessage  - the validation message
+//   #itemList      - the list container
+//   #emptyState    - the empty list message
+
+// Grab the elements using any selector: #id, .class, button, etc.
+
+const getEl = (selector) => document.querySelector(selector);
+
+// Event listeners to attach and perform actions
+
+// Add Item
+
+// Validate Item
+
+// Remove Item
+
+// Empty State
